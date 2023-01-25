2ea1aa748d830c01a8aa3a3bcff189e1cda9a3bb
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-25 12:12:03 +0200

    data files added

---
+++ food-app/assets/data/popularData.js
@@ -0,0 +1,34 @@
+const popularData = [
+  {
+    id: "popular-data-1",
+    title: "Meal 1",
+    image: require(""),
+    imageBig: require(""),
+    description: "",
+    time: "",
+    calories: "",
+    rating: "",
+  },
+  {
+    id: "popular-data-2",
+    title: "Meal 2",
+    image: require(""),
+    imageBig: require(""),
+    description: "",
+    time: "",
+    calories: "",
+    rating: "",
+  },
+  {
+    id: "popular-data-3",
+    title: "Meal 3",
+    image: require(""),
+    imageBig: require(""),
+    description: "",
+    time: "",
+    calories: "",
+    rating: "",
+  },
+];
+
+export default popularData;
---
+++ food-app/assets/data/trendingData.js
@@ -0,0 +1,34 @@
+const trendingData = [
+  {
+    id: "trending-data-1",
+    title: "Trending 1",
+    image: require(""),
+    imageBig: require(""),
+    description: "",
+    time: "",
+    calories: "",
+    rating: "",
+  },
+  {
+    id: "trending-data-2",
+    title: "Trending 2",
+    image: require(""),
+    imageBig: require(""),
+    description: "",
+    time: "",
+    calories: "",
+    rating: "",
+  },
+  {
+    id: "trending-data-3",
+    title: "Trending 3",
+    image: require(""),
+    imageBig: require(""),
+    description: "",
+    time: "",
+    calories: "",
+    rating: "",
+  },
+];
+
+export default trendingData;
