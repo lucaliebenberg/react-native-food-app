697c1c75d38e2c55cf0ead6e55a7de91bea9331c
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-25 11:51:14 +0200

    avatar add + menu add to Home component

--- food-app/screens/Home.js
+++ food-app/screens/Home.js
@@ -5,16 +5,50 @@ import {
   View,
   ScrollView,
   FlatList,
+  Image,
   ImageBackground,
   TouchableOpacity,
 } from "react-native";
 
+import Feather from "react-native-vector-icons/Feather";
+
+import Avatar from "../assets/avatar-icon.png";
+
+Feather.loadFont();
+
 function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <View style={styles.header}>
+        <View style={styles.menu}>
+          <Feather name="menu" size={32} color="black" />
+        </View>
+        <View style={styles.avatar}>
+          <ImageBackground source={Avatar} style={styles.avatar} />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    paddingVertical: 80,
+  },
+  menu: {
+    paddingVertical: 5,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+  },
+});
 
 export default Home;
